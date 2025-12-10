65045837b3e06e0f3122cbd0cd1ae3a7d3e04816
Rishabh Jaiswal <rishabhrjjaiwal@gmail.com>
2025-12-10 05:28:36 +0000

    day2

---
+++ day2.js
@@ -0,0 +1,29 @@
+let a = 22
+const b = "abc"
+var c = 78
+d = 88
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+a =50
+// b = "sd"
+c = 45
+d = 67 
+let e = "abc"
+let f = ""
+let g = null
+let h;
+// console.log("value of e" + " " +e);
+// console.log(`value of e ${e}`)
+// console.log("value of e" + " " +f);
+// console.log(`value of f ${f}`)
+// console.log("value of e" + " " +g);
+// console.log("value of e" + " " +h);
+console.log(typeof(h))
+let score = "aad" 
+console.log(typeof(score))
+console.log(typeof "score" )
+let score2 = Number(score)
+console.log(typeof(score2))
+console.log(score)
